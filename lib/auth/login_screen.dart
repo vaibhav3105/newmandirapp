e0c45ecb9c88289bb06741b628a25f581d284bdd
vaibhav3105 <jainvaibhav312003@gmail.com>
2023-06-23 19:41:52 +0530

    test

--- lib/auth/login_screen.dart
+++ lib/auth/login_screen.dart
@@ -182,7 +182,7 @@ class _LoginScreenState extends State<LoginScreen> {
                                     color: Colors.white,
                                   )
                                 : const Text(
-                                    "Login hat jaa yaha se",
+                                    "Login",
                                     style: TextStyle(
                                       color: Colors.white,
                                       fontSize: 16,
